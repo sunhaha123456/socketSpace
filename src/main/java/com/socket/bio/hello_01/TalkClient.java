4cc2fd73fd162e816a78e893b4ed4af52be49321
sunhaha123456 <771968690@qq.com>
2021-01-24 22:25:30 +0800

    Update TalkClient.java

--- src/main/java/com/socket/bio/hello_01/TalkClient.java
+++ src/main/java/com/socket/bio/hello_01/TalkClient.java
@@ -5,7 +5,39 @@ import java.net.Socket;
 
 // 同步阻塞 socket client
 public class TalkClient {
+
     public static void main(String[] args) {
+        objMsg();
+    }
+
+    public static void objMsg() {
+        Socket s = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try {
+            String ip = "127.0.0.1";
+            int port = 8080;
+            s = new Socket(ip, port);
+
+            oos = new ObjectOutputStream(s.getOutputStream());
+            oos.writeObject(new TalkServer_03.DemoTransObj("clientSendMsg-aaa", "clientSendMsg-bbb"));
+            oos.flush();
+
+            ois = new ObjectInputStream(s.getInputStream());
+            TalkServer_03.DemoTransObj readObj = (TalkServer_03.DemoTransObj) ois.readObject();
+            System.out.println("客户端收到的信息：" + readObj.getParam1() + "  " + readObj.getParam2());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void strMsg() {
         Socket s = null;
         BufferedWriter bw = null;
         BufferedReader br = null;
@@ -19,12 +51,12 @@ public class TalkClient {
             br = new BufferedReader(new InputStreamReader(s.getInputStream()));
             System.out.println("客户端收到的信息：" + br.readLine());
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         } finally {
             try {
                 s.close();
             } catch (IOException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
         }
     }
