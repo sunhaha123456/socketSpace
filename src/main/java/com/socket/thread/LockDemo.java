a0f16bc74ae4ac9d8976ab5002e1d75cad6e5d5f
sunpeng118 <sunpeng118@jd.com>
2021-07-09 13:06:47 +0800

    完善多线程

--- src/main/java/com/socket/thread/LockDemo.java
+++ src/main/java/com/socket/thread/LockDemo.java
@@ -1,11 +1,13 @@
 package com.socket.thread;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockDemo {
 
-    private Lock lock = new ReentrantLock();
+    private Lock lock = new ReentrantLock(); //默认是创建一把非公平锁
 
     public void show(String name) {
         lock.lock(); //获取锁，如果未获取到锁，会一直等待
@@ -19,10 +21,22 @@ public class LockDemo {
     }
 
     public static void main(String[] args) {
-        while (true) {
-            LockDemo ld = new LockDemo();
-            new Thread(() -> ld.show("A")).start();
-            new Thread(() -> ld.show("B")).start();
+        LockDemo ld = new LockDemo();
+
+        List<Thread> threadList = new ArrayList<Thread>();
+        threadList.add(new Thread(() -> ld.show(1 + "")));
+        threadList.add(new Thread(() -> ld.show(2 + "")));
+        threadList.add(new Thread(() -> ld.show(3 + "")));
+        threadList.add(new Thread(() -> ld.show(4 + "")));
+        threadList.add(new Thread(() -> ld.show(5 + "")));
+        threadList.add(new Thread(() -> ld.show(6 + "")));
+        threadList.add(new Thread(() -> ld.show(7 + "")));
+        threadList.add(new Thread(() -> ld.show(8 + "")));
+        threadList.add(new Thread(() -> ld.show(9 + "")));
+        threadList.add(new Thread(() -> ld.show(10 + "")));
+
+        for (Thread t : threadList) {
+            t.start();
         }
     }
 }
