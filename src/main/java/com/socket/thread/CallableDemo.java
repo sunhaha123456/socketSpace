5484bcb01f3147c0446b09319b92cade0d7388b9
sunpeng118 <sunpeng118@jd.com>
2021-11-12 11:44:20 +0800

    完善多线程callable

--- src/main/java/com/socket/thread/CallableDemo.java
+++ src/main/java/com/socket/thread/CallableDemo.java
@@ -2,21 +2,28 @@ package com.socket.thread;
 
 import java.util.concurrent.*;
 
-class TaskWithResult implements Callable<String> {
+class CallableDemo implements Callable<String> {
 
     @Override
     public String call() throws Exception {
         Thread.sleep(1000);
         return "OK";
     }
-}
 
-public class CallableDemo {
     public static void main(String[] args) throws Exception {
         ExecutorService exec = Executors.newCachedThreadPool();
-        Future<String> st = exec.submit(new TaskWithResult());
 
-        System.out.println(st.get(2000, TimeUnit.MILLISECONDS)); //同步等待结果，而且设置超时时间，注意此处一定要try捕获异常，不然会有问题
-        System.out.println("over");
+        //1、callable第一种使用方式
+        Future<String> future = exec.submit(new CallableDemo());
+        String res1 = future.get(2000, TimeUnit.MILLISECONDS); //同步等待结果，而且设置超时时间，注意此处一定要try捕获异常，不然会有问题
+        System.out.println(res1);
+
+        System.out.println("========================================");
+
+        //2、callable第二种使用方式
+        FutureTask<String> futureTask = new FutureTask(new CallableDemo());
+        exec.execute(futureTask);
+        String res2 = futureTask.get(2000, TimeUnit.MILLISECONDS); //同步等待结果，而且设置超时时间，注意此处一定要try捕获异常，不然会有问题
+        System.out.println(res2);
     }
 }
